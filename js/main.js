5aa3e714a39318a8342fca9b34713a026f4bf689
Mathieu <mathieu@MacBook-Pro-de-Mathieu.local>
2017-07-20 14:38:58 +0200

    add focus and press enter

--- js/main.js
+++ js/main.js
@@ -1,15 +1,45 @@
 var compt=0
 var pseudo = prompt("pseudo")
+
 $(document).ready(function(){
   $('#boutton').click(function() {
      humain();
-     setTimeout(bot,1000)
+     setTimeout(bot,500)
+
   });
 
   function humain(){
                    var message = $('#message').val();
                    var envoie =$('<p class="lol" ></p>').text(pseudo + " : " + message);
                    $(".bg").append(envoie)
+                   $('#message').val("")
+                };
+
+  function bot(){
+    var tab = ["bonjour", "salut", "coucou"]
+    var ajout =$('<p class="lol2"></p>').text(tab[compt]);
+    $(".bg").append(ajout)
+    compt++
+    if (compt==tab.length){
+      compt=0
+    }
+  };
+});
+
+$(document).ready(function(){
+  $('#message').keypress(function(e) {
+    if (e.keyCode == 13) {
+     humain();
+     setTimeout(bot,500)
+
+  }
+});
+
+  function humain(){
+                   var message = $('#message').val();
+                   var envoie =$('<p class="lol" ></p>').text(pseudo + " : " + message);
+                   $(".bg").append(envoie)
+                   $('#message').val("")
                 };
 
   function bot(){
