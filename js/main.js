8a875fd80a602aa144e3d230e7a0a917ef9c1d47
Mathieu <mathieu@MacBook-Pro-de-Mathieu.local>
2017-07-20 13:46:53 +0200

    finish html

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var compt=0
+var pseudo = prompt("pseudo")
+$(document).ready(function(){
+  $('#boutton').click(function() {
+     humain();
+     setTimeout(bot,1000)
+  });
+
+  function humain(){
+                   var message = $('#message').val();
+                   var envoie =$('<p class="lol" ></p>').text(pseudo + " : " + message);
+                   $(".bg").append(envoie)
+                };
+
+  function bot(){
+    var tab = ["bonjour", "salut", "coucou"]
+    var ajout =$('<p class="lol2"></p>').text(tab[compt]);
+    $(".bg").append(ajout)
+    compt++
+    if (compt==tab.length){
+      compt=0
+    }
+  };
+});
